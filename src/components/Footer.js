c1e16b4f1dbf9adb3160c1ad603b2ca4aa77cc47
Julien Corion <astram59@gmail.com>
2021-05-04 17:50:44 +0200

    language tags resized & navbar cleaned

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,12 @@ export const Footer = () => {
         
         <div>Contact : astram59@gmail.com</div>
         <ul>
+          <a href='https://www.linkedin.com/in/julien-corion-4945021b1' target='blank'>
             <li><i className="fab fa-linkedin"></i></li>
+          </a>
+          <a href='https://github.com/Mordhin' target='blank'>
             <li><i className="fab fa-github-square"></i></li>
+          </a>
             <li><i className="fab fa-twitter-square"></i></li>
         </ul>
         
